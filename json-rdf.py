82be8fa2687a96fda888bddcd3204644a81bd22f
Ioan Dragan <ioan.dumitru.dragan@gmail.com>
2016-03-04 10:56:49 +0200

    Changes with PyCharm

--- json-rdf.py
+++ json-rdf.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 """
 Copyright 2015, Institute e-Austria, Timisoara, Romania
     http://www.ieat.ro/
@@ -27,75 +27,58 @@ from rdflib import Graph
 import sys
 from pprint import pprint
 
+
 def readJson(filename):
-        with open(filename) as data_file: 
-		data = json.loads(data_file.read())
-                
-        dcterms = rdflib.Namespace("http://purl.org/dc/terms/")
-        namespace_manager = NamespaceManager(Graph())
+    with open(filename) as data_file:
+            data = json.loads(data_file.read())
 
-        rdf = rdflib.Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
-        ems = rdflib.Namespace("http://open-services.net/ns/ems#")
-        ex = rdflib.Namespace("http://example.org#")
-        xsd = rdflib.Namespace("http://www.w3.org/2001/XMLSchema#")
-        qudt = rdflib.Namespace("http://qudt.org/vocab/unit#")
-        
-        rdfs = rdflib.Namespace("http://www.w3.org/2000/01/rdf-schema#")
-        crtv = rdflib.Namespace("http://open-services.net/ns/crtv#")
-        pm = rdflib.Namespace("http://open-services.net/ns/perfmon#")
-        oslc = rdflib.Namespace("http://open-services.net/ns/core#")
-        bp = rdflib.Namespace("http://open-services.net/ns/basicProfile#")
-        dbp = rdflib.Namespace("http://dbpedia.org/resource/")
+    dcterms = rdflib.Namespace("http://purl.org/dc/terms/")
+    namespace_manager = NamespaceManager(Graph())
 
-        jsonUsefulData = data['hits']['hits']
-        g = Graph()
-        
-        for d in jsonUsefulData: 
-                if d['_source']['plugin'] == 'load':
-                        print d['_source']['shortterm']
-                        print d['_source']['longterm']
-                        print d['_source']['midterm']
-                elif d['_source']['plugin'] == 'interface':
-                        print d['_source']['rx']
-                        print d['_source']['tx']
-                elif d['_source']['plugin'] == 'cpu':
-                        print "CPU DATA"
-                        cpu = BNode()
-                        g.add( (rdflib.URIRef('cpu'), RDFS.label, rdflib.Literal('cpu')) )
-                        g.add( (cpu, RDFS.label, rdflib.Literal('cpu metric')) )
-                        g.add( (cpu, pm.CpuUsed, rdflib.Literal(d['_source']['value'])) )
-                        
-                        print d['_source']['value']
-                        print d['_source']['plugin_instance']
-                        print d['_source']['type_instance']
-                        print "end CPU data"
-                else:
-                        print d['_source']['value']
-                        print d['_source']['type_instance']
+    rdf = rdflib.Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
+    ems = rdflib.Namespace("http://open-services.net/ns/ems#")
+    ex = rdflib.Namespace("http://example.org#")
+    xsd = rdflib.Namespace("http://www.w3.org/2001/XMLSchema#")
+    qudt = rdflib.Namespace("http://qudt.org/vocab/unit#")
 
-        
+    rdfs = rdflib.Namespace("http://www.w3.org/2000/01/rdf-schema#")
+    crtv = rdflib.Namespace("http://open-services.net/ns/crtv#")
+    pm = rdflib.Namespace("http://open-services.net/ns/perfmon#")
+    oslc = rdflib.Namespace("http://open-services.net/ns/core#")
+    bp = rdflib.Namespace("http://open-services.net/ns/basicProfile#")
+    dbp = rdflib.Namespace("http://dbpedia.org/resource/")
+
+    jsonUsefulData = data['hits']['hits']
+    g = Graph()
+
+    for d in jsonUsefulData:
+        if d['_source']['plugin'] == 'load':
+            print d['_source']['shortterm']
+            print d['_source']['longterm']
+            print d['_source']['midterm']
+        elif d['_source']['plugin'] == 'interface':
+            print d['_source']['rx']
+            print d['_source']['tx']
+        elif d['_source']['plugin'] == 'cpu':
+            print "CPU DATA"
+            cpu = BNode()
+            g.add( (rdflib.URIRef('cpu'), RDFS.label, rdflib.Literal('cpu')) )
+            g.add( (cpu, RDFS.label, rdflib.Literal('cpu metric')) )
+            g.add( (cpu, pm.CpuUsed, rdflib.Literal(d['_source']['value'])) )
+
+            print d['_source']['value']
+            print d['_source']['plugin_instance']
+            print d['_source']['type_instance']
+            print "end CPU data"
+        else:
+            print d['_source']['value']
+            print d['_source']['type_instance']
+
+
         print g.serialize(format = 'xml')
 
-        
-def dummy():
-        for maps in data['maps']: 
-		#get in the maps and collect attributes 
-		for i in maps:
- 			g.add( (rdflib.URIRef('maps.'+str(counter)), pm.AvgLoginRequestFailures, rdflib.Literal(i) ) ) 
-                counter= counter+1
-		print len(maps)
 
-	#g.add((rdflib.URIRef('maps:id'), rdflib.RDFS.label, rdflib.Literal('id')))
-	#g.add((rdflib.URIRef('maps:id'), FOAF.value, rdflib.Literal(12)))
-	print g.serialize(format = 'xml')
-
-
-def printBob():
-	g = rdflib.Graph()
-	g.add((rdflib.URIRef('urn:bob'), rdflib.RDFS.label, rdflib.Literal('bob')))
-	print g.serialize(format = 'xml')
-	
 if __name__ == '__main__':
-	print sys.argv[1]
-	readJson(sys.argv[1])
-	
+    print sys.argv[1]
+    readJson(sys.argv[1])
+
